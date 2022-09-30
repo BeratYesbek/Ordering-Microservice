b130f7f0b1b631963930b7384c202449f0e9df2b
Feanor <beratyesbek@gmail.com>
2022-09-30 12:05:35 +0300

    Exception Middleware was initialized

---
+++ Infrastructure/CrossCuttingConcerns/Exceptions/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.CrossCuttingConcerns.Exceptions.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exception)
+            {
+                await HandleExceptionAsync(context, exception);
+            }
+        }
+
+        public Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            if (exception.GetType() == typeof(ValidationException)) return CreateValidationException(context, exception);
+            return CreateInternalServerError(context, exception);
+        }
+
+        private Task CreateValidationException(HttpContext context, Exception exception)
+        {
+            context.Response.StatusCode = Convert.ToInt32(HttpStatusCode.BadRequest);
+            object errors = ((ValidationException)exception).Errors;
+
+            return context.Response.WriteAsync(new
+            {
+                Status = StatusCodes.Status400BadRequest,
+                Errors = errors
+            }.ToString()!);
+        }
+
+        public Task CreateInternalServerError(HttpContext context, Exception exception)
+        {
+            context.Response.StatusCode = Convert.ToInt32(HttpStatusCode.InternalServerError);
+
+            return context.Response.WriteAsync(new
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Message = exception.Message
+            }.ToString()!);
+        }
+
+    }
+}
